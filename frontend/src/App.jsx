3290b3c77a20064c1f943a6fa22901624b1a81db
Prashant Singh Chouhan <90359988+Prashantsinghchouhan@users.noreply.github.com>
2025-06-10 01:16:19 +0530

    Added react query for api fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
+import { useState } from "react";
+import { PingComponent } from "./components/atoms/pingComponent";
+import usePing from "./hooks/apis/queries/usePing";
 
-function App() {}
+function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <>
+      <button onClick={() => setIsVisible(!isVisible)}> Toogle</button>
+      {isVisible && <PingComponent />}
+    </>
+  );
+}
 
 export default App;
