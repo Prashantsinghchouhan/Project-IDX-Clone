13e275d3592e667df7e55a837d5d97551cb9ed45
Prashant Singh Chouhan <90359988+Prashantsinghchouhan@users.noreply.github.com>
2025-06-09 23:43:23 +0530

    Added basic backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config/serverConfig.js";
+const app = express();
+
+//body parser used to accept the request body
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.get("/ping", (req, res) => {
+  return res.json({ message: "pong" });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
